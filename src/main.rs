f0d1a21693f0fca25c67a1c2f5405dddde042d0a
Raj Ramalingam <rajramalingam@rajs-mbp.lan>
2024-11-28 15:34:01 -0800

    Added package

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 
-
-use rand::{thread_rng,seq::SliceRandom};
-
 #[derive(Debug)]
 struct Deck{
     cards: Vec<String>
@@ -29,9 +26,8 @@ impl Deck {
         Deck { cards } // Implicit return 
     }
 
-    fn shuffle(&mut self) {
-        let mut rng = thread_rng();
-        self.cards.shuffle(&mut rng);
+    fn deal(&mut self,num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
     }
     
 }
@@ -40,7 +36,9 @@ impl Deck {
 fn main() {
     let mut deck = Deck::new();
    
-    deck.shuffle();
-    println!("Heres your deck {:#?}", deck)
+    let cards = deck.deal(2);
+    // Error handling .. to be added.
+    println!("Heres your deck {:#?}", deck);
+    println!("Heres your hand {:#?}", cards);
 
 }
